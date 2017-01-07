3a1d93b693dfc0a1ce0f24fe796c8282aab7aa2a
Dolph Mathews <dolph.mathews@gmail.com>
2017-01-07 10:48:08 -0600

    Install rustc from package

--- ci/build.sh
+++ ci/build.sh
@@ -6,10 +6,7 @@ DIR=`dirname $(readlink -f $0)`
 
 # Install dependencies.
 apt-get update
-apt-get install -y curl
-
-# Install rust.
-curl https://sh.rustup.rs -sSf | sh
+apt-get install -y rustc
 
 # Build the project.
 rustc $DIR/src/main.rs
