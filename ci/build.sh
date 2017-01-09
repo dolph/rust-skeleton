02f32ac38f3afe8dddc8b28b7cf2093b31425773
Dolph Mathews <dolph.mathews@gmail.com>
2017-01-09 11:09:20 -0600

    Add argument parser

--- ci/build.sh
+++ ci/build.sh
@@ -21,3 +21,6 @@ cargo build --release
 
 # Smoke test the result.
 cargo run
+./target/debug/hello_world
+./target/debug/hello_world --help
+./target/debug/hello_world --verbose
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate argparse;
+
+use argparse::{ArgumentParser, StoreTrue, Store};
+
 fn main() {
-    println!("Hello, world!");
+    let mut verbose = false;
+
+    { // Limit the scope of borrows by the ap.refer() method.
+        let mut parser = ArgumentParser::new();
+        parser.set_description("Hello, world.");
+        parser.refer(&mut verbose)
+            .add_option(&["-v", "--verbose"], StoreTrue,
+            "Enable verbose output.");
+        parser.parse_args_or_exit();
+    }
+
+    if verbose {
+        println!("Hello, world.");
+    }
 }
